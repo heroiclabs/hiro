8b2186207e968a455766167818f62d74666ada5f
Andrei Mihu <andrei@heroiclabs.com>
2024-11-06 15:52:48 +0000

    Additional stats configuration options.

--- stats.go
+++ stats.go
@@ -22,14 +22,20 @@ import (
 
 // StatsConfig is the data definition for a StatsSystem type.
 type StatsConfig struct {
-	Whitelist []string `json:"whitelist,omitempty"`
+	Whitelist    []string                    `json:"whitelist,omitempty"`
+	StatsPublic  map[string]*StatsConfigStat `json:"stats_public,omitempty"`
+	StatsPrivate map[string]*StatsConfigStat `json:"stats_private,omitempty"`
+}
+
+type StatsConfigStat struct {
+	Value int64 `json:"value,omitempty"`
 }
 
 type StatsSystem interface {
 	System
 
 	// List all private stats for one or more users.
-	List(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userIDs []string) (stats map[string]*StatList, err error)
+	List(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userID string, userIDs []string) (stats map[string]*StatList, err error)
 
 	// Update private stats for a particular user.
 	Update(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userID string, publicStats []*StatUpdate, privateStats []*StatUpdate) (stats *StatList, err error)
