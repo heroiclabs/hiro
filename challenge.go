c7ed4e639ed47caced71b2b9006ac0d6b19548b1
Simon Esposito <simon@heroiclabs.com>
2025-04-17 13:16:02 +0100

    Challenge proto changes

--- challenge.go
+++ challenge.go
@@ -15,6 +15,8 @@ type ChallengesConfigChallenge struct {
 	MaxNumScore          int64                                  `json:"max_num_score,omitempty"`
 	StartDelayMaxSec     int64                                  `json:"start_delay_max_sec,omitempty"`
 	Open                 bool                                   `json:"open,omitempty"`
+	Ascending            bool                                   `json:"ascending,omitempty"`
+	Operator             string                                 `json:"operator,omitempty"`
 	Duration             *ChallengeDuration                     `json:"duration,omitempty"`
 	Players              *ChallengesConfigPlayers               `json:"players,omitempty"`
 }
@@ -38,20 +40,30 @@ type ChallengesConfigPlayers struct {
 type ChallengeSystem interface {
 	System
 
-	// GetTemplates lists all available auction configurations that can be used to create auction listings.
+	// GetTemplates lists all available challenge configurations that can be used to create new challenges.
 	GetTemplates(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userID string) (*ChallengeTemplates, error)
 
-	GetChallenge(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, challengeId string) (*Challenge, error)
+	// GetChallenge returns a challenge the user has been invited to or which is participating in.
+	GetChallenge(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, challengeId, userId string) (*Challenge, error)
 
-	ListChallenges(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userID string) ([]*Challenge, error)
+	// ListChallenges Lists all the user's pending or joined challenges.
+	ListChallenges(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userId string, categories []string) ([]*Challenge, error)
 
-	CreateChallenge(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userID, name, description string, startTimeSec, durationSec int64, invitees []string, ascending, open bool, maxParticipants, maxScores int64) (*Challenge, error)
+	// CreateChallenge a new challenge for a list of users.
+	CreateChallenge(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userId, templateId, name, description, category string, startDelaySec, durationSec int64, invitees []string, maxPlayers int64) (*Challenge, error)
 
+	// JoinChallenge Joins a challenge the user's been invited to.
 	JoinChallenge(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userId, challengeId string) (*Challenge, error)
 
+	// LeaveChallenge rejects a challenge invitation or abandons a joined challenge.
 	LeaveChallenge(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userId, challengeId string) (*Challenge, error)
 
-	SubmitChallengeScore(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userId, username, displayName, avatarUrl, challengeId string, score, subscore int64, metadata map[string]any) (challenge *Challenge, err error)
+	// SubmitChallengeScore submits a new score to the challenge.
+	SubmitChallengeScore(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userId, challengeId string, score, subscore int64, metadata map[string]any) (challenge *Challenge, err error)
 
-	ClaimChallenge(ctx context.Context, userId, challengeId string) (*Challenge, error)
+	// ClaimChallenge claims a reward of a challenge, if any.
+	ClaimChallenge(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userId, challengeId string) (*Challenge, error)
+
+	// SetOnChallengeReward sets a custom reward function which will run after a challenge reward has been claimed.
+	SetOnChallengeReward(fn OnReward[*Challenge])
 }
